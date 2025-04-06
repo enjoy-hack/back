6c9b99ff1f4e71b7012d87e512e9e7db6ce206ba
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-06 13:19:49 +0900

    Refactor : User 패키지 구조 변경에 따른 임포트 수정

--- src/main/java/com/example/smartair/repository/UserRepository.java
+++ src/main/java/com/example/smartair/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.smartair.repository;
 
-import com.example.smartair.entity.User;
+import com.example.smartair.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
