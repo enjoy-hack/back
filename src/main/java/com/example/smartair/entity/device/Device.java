24d548e9c311503de5d29bc860c00c08c6f24817
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-07 22:35:08 +0900

    Chore : 생성자 어노테이션 추가

--- src/main/java/com/example/smartair/entity/device/Device.java
+++ src/main/java/com/example/smartair/entity/device/Device.java
@@ -7,13 +7,19 @@ import com.example.smartair.entity.airData.AirQualityData;
 import com.example.smartair.entity.airData.FineParticlesData;
 import com.example.smartair.util.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Device extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +32,6 @@ public class Device extends BaseEntity {
     @ManyToOne //기기와 유저 : 다대일 관계
     @JoinColumn(name = "user_id")
     private User user;
+
+
 }
--- src/main/java/com/example/smartair/entity/room/Room.java
+++ src/main/java/com/example/smartair/entity/room/Room.java
@@ -5,11 +5,17 @@ import com.example.smartair.entity.roomDevice.RoomDevice;
 import com.example.smartair.entity.user.User;
 import com.example.smartair.util.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Room extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
