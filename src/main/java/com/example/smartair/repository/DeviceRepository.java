642a099ed930dc747cf3e04a6d58733da106cbb4
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-07 11:23:06 +0900

    Feat : DeviceRepository 생성

---
+++ src/main/java/com/example/smartair/repository/DeviceRepository.java
@@ -0,0 +1,12 @@
+package com.example.smartair.repository;
+
+import com.example.smartair.entity.device.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+    Optional<Device> findById(Long id);
+}
