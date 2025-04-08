fbbf48b7a47b6a04c0f80483ef875924251f3d7b
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-08 21:19:56 +0900

    Fix : 불필요한 선언 삭제

--- src/main/java/com/example/smartair/entity/airScore/RoomAirQualityScore.java
+++ src/main/java/com/example/smartair/entity/airScore/RoomAirQualityScore.java
@@ -11,7 +11,6 @@ public class RoomAirQualityScore extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private
     private double averageScore;
 
     @ManyToOne
