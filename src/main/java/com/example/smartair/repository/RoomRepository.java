9fb6acde4f15665a7d05ea6eb309856616405bcd
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-08 19:44:47 +0900

    Feat : RoomRepository 생성

--- src/main/java/com/example/smartair/repository/RoomRepository.java
+++ src/main/java/com/example/smartair/repository/RoomRepository.java
@@ -1,9 +1,13 @@
 package com.example.smartair.repository;
 
+import com.example.smartair.entity.place.Place;
 import com.example.smartair.entity.room.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    List<Room> findAllByPlace(Place place);
 }
