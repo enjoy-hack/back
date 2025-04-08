e73c073c732aaf76eac11d30b5eb718f7581028a
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-08 19:45:24 +0900

    Chore : 패키지명 변경에 따른 임포트 변경

--- src/main/java/com/example/smartair/service/MqttService/MqttReceiveService.java
+++ src/main/java/com/example/smartair/service/MqttService/MqttReceiveService.java
@@ -1,6 +1,6 @@
 package com.example.smartair.service.mqttService;
 
-import com.example.smartair.dto.airQualityDto.AirQualityPayloadDto;
+import com.example.smartair.dto.AirQualityDto.AirQualityPayloadDto;
 import com.example.smartair.entity.airData.AirQualityData;
 import com.example.smartair.service.airQualityService.AirQualityDataService;
 import org.springframework.stereotype.Service;
