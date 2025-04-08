eff9dc8d330fce539589d42978e6630241cd6f72
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-08 20:57:03 +0900

    Update : 에러 코드 추가

--- src/main/java/com/example/smartair/exception/ErrorCode.java
+++ src/main/java/com/example/smartair/exception/ErrorCode.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청이 들어왔습니다");
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청이 들어왔습니다"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+    DEVICE_NOT_FOUND(HttpStatus.NOT_FOUND, "기기를 찾을 수 없습니다."),
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 방입니다."),
+    PLACE_NOT_FOUND(HttpStatus.NOT_FOUND, "등록되지 않은 공간입니다.");
 
     private final HttpStatus status;
     private final String message;
