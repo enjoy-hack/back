81758855c85e8131996a63c895e8ca22c86711ca
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-29 21:06:41 +0900

    Feat : RoomAirQualityScore 필드 추가

--- src/main/java/com/example/smartair/entity/airScore/RoomAirQualityScore.java
+++ src/main/java/com/example/smartair/entity/airScore/RoomAirQualityScore.java
@@ -4,16 +4,26 @@ import com.example.smartair.entity.place.Place;
 import com.example.smartair.entity.room.Room;
 import com.example.smartair.util.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class RoomAirQualityScore extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private double averageScore;
+    private double overallScore;
+    private double pm10Score;
+    private double pm25Score;
+    private double eco2Score;
+    private double tvocScore;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id")
     private Room room;
 
