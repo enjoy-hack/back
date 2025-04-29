aebb48bd5ed6c3078510d465976ef13b3a000f08
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-29 21:02:55 +0900

    Refactor : AirQualityScore->DeviceAirQualityScore로 이름 변경

--- src/main/java/com/example/smartair/entity/airScore/DeviceAirQualityScore.java
+++ src/main/java/com/example/smartair/entity/airScore/DeviceAirQualityScore.java
@@ -3,19 +3,29 @@ package com.example.smartair.entity.airScore;
 import com.example.smartair.entity.airData.AirQualityData;
 import com.example.smartair.util.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-public class AirQualityScore extends BaseEntity {
+@Setter
+@Getter
+@NoArgsConstructor
+public class DeviceAirQualityScore extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
+    @JoinColumn(name = "air_quality_data_id")
     private AirQualityData airQualityData;
 
-    private double score;
+    private double overallScore;
 
     private double pm10Score;
     private double pm25Score;
+    private double eco2Score;
     private double tvocScore;
+
+
 }
