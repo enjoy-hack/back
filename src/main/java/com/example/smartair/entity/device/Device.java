117d4b8c7c5c95b68f4c4b950302b7e1887a2381
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-04-30 18:39:50 +0900

    Feat : Device와 Room 단방향 관계 추가

--- src/main/java/com/example/smartair/entity/device/Device.java
+++ src/main/java/com/example/smartair/entity/device/Device.java
@@ -1,19 +1,13 @@
 package com.example.smartair.entity.device;
 
-import com.example.smartair.entity.hvacSetting.HvacSetting;
-import com.example.smartair.entity.roomDevice.RoomDevice;
+import com.example.smartair.entity.room.Room;
 import com.example.smartair.entity.user.User;
-import com.example.smartair.entity.airData.AirQualityData;
-import com.example.smartair.entity.airData.FineParticlesData;
 import com.example.smartair.util.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -32,6 +26,10 @@ public class Device extends BaseEntity {
     @ManyToOne //기기와 유저 : 다대일 관계
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne //기기와 방 : 다대일 관계
+    @JoinColumn(name = "room_id")
+    private Room room;
 
 
 }
