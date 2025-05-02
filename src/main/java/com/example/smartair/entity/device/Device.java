7adde7381aae7cf82719b98ff984a831f425d42c
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-02 18:47:41 +0900

    Update : Device의 Room매핑 삭제

--- src/main/java/com/example/smartair/entity/device/Device.java
+++ src/main/java/com/example/smartair/entity/device/Device.java
@@ -1,6 +1,5 @@
 package com.example.smartair.entity.device;
 
-import com.example.smartair.entity.room.Room;
 import com.example.smartair.entity.user.User;
 import com.example.smartair.util.BaseEntity;
 import jakarta.persistence.*;
@@ -26,10 +25,5 @@ public class Device extends BaseEntity {
     @ManyToOne //기기와 유저 : 다대일 관계
     @JoinColumn(name = "user_id")
     private User user;
-
-    @ManyToOne //기기와 방 : 다대일 관계
-    @JoinColumn(name = "room_id")
-    private Room room;
-
 
 }
