797fbde815b141498f8a674e0f91399b09694dbb
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-05 12:18:05 +0900

    Chore : 자료형 Long으로 수정

--- src/main/java/com/example/smartair/repository/airQualityDataRepository/RoomAirQualityDataRepository.java
+++ src/main/java/com/example/smartair/repository/airQualityDataRepository/RoomAirQualityDataRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomAirQualityDataRepository extends JpaRepository<RoomAirQualityData, Integer> {
+public interface RoomAirQualityDataRepository extends JpaRepository<RoomAirQualityData, Long> {
 }
