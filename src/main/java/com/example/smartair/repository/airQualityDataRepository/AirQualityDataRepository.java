be833ab81e664f45b83d05ff31535ca1690390c0
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-06 15:29:28 +0900

    Feat : 최신 7개 데이터 조회 쿼리 추가

--- src/main/java/com/example/smartair/repository/airQualityDataRepository/AirQualityDataRepository.java
+++ src/main/java/com/example/smartair/repository/airQualityDataRepository/AirQualityDataRepository.java
@@ -4,7 +4,11 @@ import com.example.smartair.entity.airData.airQualityData.DeviceAirQualityData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AirQualityDataRepository extends JpaRepository<DeviceAirQualityData, Long> {
+
+    List<DeviceAirQualityData> findTop7ByDeviceIdOrderByCreatedAtDesc(Long deviceId);
 
 }
