e60d4c098bb185e5b498e87c718234014c538fa6
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-06 15:31:40 +0900

    Feat : 방에 존재하는 모든 디바이스 조회 쿼리 추가

--- src/main/java/com/example/smartair/repository/roomDeviceRepository/RoomDeviceRepository.java
+++ src/main/java/com/example/smartair/repository/roomDeviceRepository/RoomDeviceRepository.java
@@ -1,13 +1,17 @@
 package com.example.smartair.repository.roomDeviceRepository;
 
 import com.example.smartair.entity.device.Device;
+import com.example.smartair.entity.room.Room;
 import com.example.smartair.entity.roomDevice.RoomDevice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoomDeviceRepository extends JpaRepository<RoomDevice, Long> {
     Optional<RoomDevice> findByDevice(Device device);
+
+    List<Device> findAllDeviceByRoom(Room room);
 }
