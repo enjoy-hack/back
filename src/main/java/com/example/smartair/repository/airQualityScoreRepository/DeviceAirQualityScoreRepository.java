f2cc2f614b3b43f15f2d8fcbb7806d9fedaa58bd
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-06 15:29:53 +0900

    Feat : 디바이스의 최신 1개 데이터 조회 쿼리 추가

--- src/main/java/com/example/smartair/repository/airQualityScoreRepository/DeviceAirQualityScoreRepository.java
+++ src/main/java/com/example/smartair/repository/airQualityScoreRepository/DeviceAirQualityScoreRepository.java
@@ -27,4 +27,6 @@ public interface DeviceAirQualityScoreRepository extends JpaRepository<DeviceAir
             Pageable pageable
     );
 
+    Optional<DeviceAirQualityScore> findTopByDeviceAirQualityData_DeviceOrderByCreatedAtDesc(Device device);
+
 } 
