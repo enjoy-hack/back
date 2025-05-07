08bd25cef0007159691001297b01a8307bd37ba8
saeeyes <133561400+saeeyes@users.noreply.github.com>
2025-05-07 15:26:14 +0900

    chore : s3 업로드 ítopic 수정

--- src/main/java/com/example/smartair/controller/deviceContoller/DeviceController.java
+++ src/main/java/com/example/smartair/controller/deviceContoller/DeviceController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -46,5 +43,6 @@ public class DeviceController {
         return ResponseEntity.ok("success");
     }
 
-
+    @GetMapping
+    public ResponseEntity<?>getDeviceList(@AuthenticationPrincipal CustomUserDetails userDetails)
 }
