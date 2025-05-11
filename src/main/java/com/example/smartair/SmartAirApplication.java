469ddb74aea6b035b0fcc258870c50315a5b4ee4
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-11 12:52:58 +0900

    Update SmartAirApplication.java

--- src/main/java/com/example/smartair/SmartAirApplication.java
+++ src/main/java/com/example/smartair/SmartAirApplication.java
@@ -2,6 +2,7 @@ package com.example.smartair;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EntityScan(basePackages = {"com.example.smartair.entity"})
 public class SmartAirApplication {
 
 	public static void main(String[] args) {
