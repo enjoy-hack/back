721375d790768eb3de24117b132e350a853b7fb6
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-12 19:38:35 +0900

    Delete : 잘못된 파일 삭제

--- src/main/java/com/example/smartair/repository/deviceRepository/deviceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.smartair.repository.deviceRepository;
-
-import com.example.smartair.entity.device.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeviceRepository extends JpaRepository<Device, Long> {
-}
