050e567b45e515299ce8c31e245a42d6dcfd8fb9
saeeyes <133561400+saeeyes@users.noreply.github.com>
2025-05-18 20:35:44 +0900

    feat : security 권한 경로 설정

--- src/main/java/com/example/smartair/config/SecurityConfig.java
+++ src/main/java/com/example/smartair/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.example.smartair.repository.userRepository.UserRepository;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -70,7 +71,11 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/login", "/join", "/reissue", "/oauth2/**",
                                 "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**"
+
                         ).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/sensorMappingWithRoom").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/anomaly").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/predictedAirQuality").permitAll()
                         .anyRequest().permitAll() //개발 중
                 );
 
