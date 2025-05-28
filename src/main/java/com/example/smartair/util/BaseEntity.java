e97ff346354b358cceb6b7caf3f4cd7dbb477f85
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-05-28 23:26:49 +0900

    Feat : BaseEntity 수정

--- src/main/java/com/example/smartair/util/BaseEntity.java
+++ src/main/java/com/example/smartair/util/BaseEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -13,11 +14,11 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public class BaseEntity {
+public abstract class BaseEntity {
     @CreatedDate
     @Column(name = "created_at", updatable= false)
     private LocalDateTime createdAt;
-    @LastModifiedBy
+    @LastModifiedDate
     @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
 }
