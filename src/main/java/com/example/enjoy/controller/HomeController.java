2e74f95a4cab903522777fbb84fc3e35b6239626
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-06-25 16:31:08 +0900

    Update : RestController 추가

--- src/main/java/com/example/enjoy/controller/HomeController.java
+++ src/main/java/com/example/enjoy/controller/HomeController.java
@@ -7,10 +7,11 @@ import com.example.enjoy.service.TrackService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class HomeController {
 
